36eece5ee6691252604d2a3578fcc027d1aa5146
computerkingminsu <ya5656@naver.com>
2024-05-26 01:11:39 +0900

    feat: 자동 음성 녹음 -> 텍스트 변환

--- src/app/test1/page.tsx
+++ src/app/test1/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Image from 'next/image';
 import { questions } from './question';
 import { useRouter } from 'next/navigation';
@@ -10,9 +10,50 @@ export default function Test1() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [selectedOption, setSelectedOption] = useState<number | null>(null);
   const [currentQuestion, setCurrentQuestion] = useState(0);
-
+  const [isRecording, setIsRecording] = useState(false);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
   const router = useRouter();
   const progressValue = ((currentQuestion + 1) / questions.length) * 100;
+
+  //녹음 시작
+  const startListening = () => {
+    if (
+      !('webkitSpeechRecognition' in window) &&
+      !('SpeechRecognition' in window)
+    ) {
+      console.log('Web Speech API is not supported by this browser.');
+      return;
+    }
+
+    const SpeechRecognition =
+      window.webkitSpeechRecognition || window.SpeechRecognition;
+    const recognition = new SpeechRecognition();
+    recognition.lang = 'ko-KR'; // 언어 설정
+    recognition.interimResults = false;
+    recognition.maxAlternatives = 1;
+
+    recognition.onstart = () => {
+      setIsRecording(true);
+      console.log('Listening...');
+    };
+
+    recognition.onresult = (event: SpeechRecognitionEvent) => {
+      const transcript: string = event.results[0][0].transcript;
+      console.log('Transcript:', transcript);
+      setIsRecording(false);
+    };
+
+    recognition.onerror = (event: SpeechRecognitionErrorEvent) => {
+      console.error('Recognition error:', event.error);
+      setIsRecording(false);
+    };
+
+    recognition.onend = () => {
+      setIsRecording(false);
+    };
+
+    recognition.start();
+  };
 
   useEffect(() => {
     const savedOption = localStorage.getItem(`question-${currentQuestion}`);
@@ -21,15 +62,37 @@ export default function Test1() {
     } else {
       setSelectedOption(null);
     }
+    // const newAudio = new Audio(`/audio/question-${currentQuestion}.mp3`);
+    const newAudio = new Audio(`/audio/question-0.mp3`);
+    audioRef.current = newAudio;
+
+    const handleCanPlayThrough = () => {
+      newAudio.play();
+      startListening(); // 녹음시작 , 사용자가 텍스트를 읽고 바로 녹음을 시작할 경우를 대비하여 바로 시작
+      setIsPlaying(true);
+    };
+
+    const handleEnded = () => {
+      setIsPlaying(false);
+    };
+
+    const handlePause = () => {
+      setIsPlaying(false);
+    };
+
+    newAudio.addEventListener('canplaythrough', handleCanPlayThrough);
+    newAudio.addEventListener('ended', handleEnded);
+    newAudio.addEventListener('pause', handlePause);
+
+    return () => {
+      newAudio.removeEventListener('canplaythrough', handleCanPlayThrough);
+      newAudio.removeEventListener('ended', handleEnded);
+      newAudio.removeEventListener('pause', handlePause);
+      newAudio.pause();
+      newAudio.currentTime = 0;
+    };
+    // }
   }, [currentQuestion]);
-
-  const handlePlay = () => {
-    setIsPlaying(true);
-  };
-
-  const handleStop = () => {
-    setIsPlaying(false);
-  };
 
   const handleOptionClick = (index: number) => {
     setSelectedOption(index);
@@ -41,6 +104,7 @@ export default function Test1() {
       setCurrentQuestion((prev) => prev + 1);
     }
   };
+
   const handlePreviousQuestion = () => {
     if (currentQuestion === 0) {
       router.push('/');
@@ -48,49 +112,23 @@ export default function Test1() {
       setCurrentQuestion((prev) => prev - 1);
     }
   };
+
   const handleSubmit = () => {
     console.log('제출');
   };
-
   return (
     <>
       <div className="px-10 mt-[5vh] ">
-        {/* 진행 프로그레스 바 */}
         <Progress value={progressValue} />
-        {/* 질문 */}
         <div className="font-semibold text-lg text-cyan-900 mt-[5vh]">
           <span>{questions[currentQuestion].question}</span>
         </div>
-        {/* 음성 대화 gif , 현재는 마우스 올렸을때 gif 실행 */}
         <div className="mt-7 flex justify-center">
-          <div
-            className="relative w-64 h-64"
-            onMouseEnter={handlePlay}
-            onMouseLeave={handleStop}
-          >
-            {/*  이거는 정지  or  움직임 */}
-            {/* {isPlaying ? (
-              <Image
-                src="/test/talking.gif"
-                layout="fill"
-                objectFit="cover"
-                alt="Animated"
-              />
-            ) : (
-              <Image
-                src="/test/talking.png"
-                layout="fill"
-                objectFit="cover"
-                alt="Static"
-              />
-            )} */}
-            {/*  이건 디폴트로 움직이고 말하고 있을때 스케일 업다운 */}
+          <div className="relative w-64 h-64">
             <div
               className={`relative w-64 h-64 ${
-                isPlaying ? 'animate-scale' : ''
+                isPlaying || isRecording ? 'animate-scale' : ''
               }`}
-              onMouseEnter={handlePlay}
-              onMouseLeave={handleStop}
             >
               <Image
                 src="/test/talking.gif"
@@ -101,13 +139,11 @@ export default function Test1() {
             </div>
           </div>
         </div>
-        {/* 사용자의 음성인식이 실패했을때 나올예정. 현재는 일단 나오게 함. */}
         <div className="mt-10 flex justify-between items-center font-semibold text-cyan-900">
           <span>전혀 아니다</span>
           <span>보통</span>
           <span>매우 그렇다</span>
         </div>
-        {/* 선택 동그라미 */}
         <div className="mt-5 flex justify-between space-x-4 items-center relative">
           {[0, 1, 2, 3, 4].map((index) => (
             <div
@@ -137,7 +173,6 @@ export default function Test1() {
             ></div>
           ))}
         </div>
-        {/* 이전, 다음 버튼 */}
         <div className="mt-24 flex justify-between font-semibold">
           <button
             className="w-40 h-12 bg-white border-2 border-[#6cd9e7] text-cyan-800 rounded-full"
